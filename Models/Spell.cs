c3a346422f60ba31148ac6d439eaad2e24109de6
Charlie Lashmar <charlie@lashmar.com>
2024-12-20 11:20:14 +0000

    fixed deserialisation

--- Models/Spell.cs
+++ Models/Spell.cs
@@ -5,12 +5,12 @@ namespace advanced_APIS.Models
     public class Spell
     {
         [JsonPropertyName("id")]
-        int Id { get; set; }
+        public int Id { get; set; }
         [JsonPropertyName("name")]
-        string? Name { get; set; } = "";
+        public string? Name { get; set; } = "";
         [JsonPropertyName("description")]
-        string? Description { get; set; } = "";
+        public string? Description { get; set; } = "";
         [JsonPropertyName("casting_instructions")]
-        string? CastingInstructions { get; set; } = "";
+        public string? CastingInstructions { get; set; } = "";
     }
 }
